2f21ed9bd0eecdfbc3cd2cb1d7d91fb47d02d43b
TheDemonSlayer <shaaravagrawal@Shaaravs-MacBook-Pro.local>
2021-03-06 16:41:27 +0530

    Platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(150,305,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -46,6 +46,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
